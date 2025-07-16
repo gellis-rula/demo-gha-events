a2c5f8c7b5eb8ff7d1f29246b0880adb8c7ebebd
Gary Ellis <gary.ellis@rula.com>
2025-07-16 09:13:27 -0700

    setup dependency install

--- src/main.go
+++ src/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-    fmt.Println("hello world")
+	var rootCmd = &cobra.Command{
+		Use:   "myapp",
+		Short: "A simple Cobra CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("hello cobra")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
